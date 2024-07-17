5f5a6ea4f13d3d47a87f1b4792835960aa11aeaa
floweryclover <floweryclover@gmail.com>
2024-07-17 17:04:35 +0900

    초기 커밋

---
+++ SyncWorld2DServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Sockets;
+
+Console.WriteLine("Hello");
+
+
+var tcpListener = TcpListener.Create(31415);
+tcpListener.Start(8);
+
+while (true)
+{
+    using var tcpClient = tcpListener.AcceptTcpClient();
+    Console.WriteLine(tcpClient.Client.RemoteEndPoint?.ToString());
+}
